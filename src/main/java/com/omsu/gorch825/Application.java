d06c8a0af6ad857075a5f8da58f59f9494887b33
Maxim Gorchitskiy <mgorchitskiy@mail.ru>
2026-05-16 08:41:58 +0600

    Конфигурация

---
+++ src/main/java/com/omsu/gorch825/Application.java
@@ -0,0 +1,20 @@
+package com.omsu.gorch825;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@SpringBootApplication
+public class Application {
+    @RequestMapping("/")
+    String home() {
+        return "Hello world!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
