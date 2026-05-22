a3eae16b3f84ed401038aa9a0ffc59383c79c0e3
Maxim Gorchitskiy <mgorchitskiy@mail.ru>
2026-05-22 15:17:32 +0600

    first migration

---
+++ src/main/resources/db/migration/V1_1_0__my_first_migration.sql
@@ -0,0 +1,56 @@
+CREATE TABLE student_group(
+    group_id BIGSERIAL PRIMARY KEY,
+    group_name VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+);
+CREATE TABLE student(
+    student_id BIGSERIAL PRIMARY KEY,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    middle_name VARCHAR(100) NOT NULL,
+
+    group_id BIGINT NOT NULL REFERENCES student_group(group_id),
+    
+    status VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+);
+CREATE TABLE teacher(
+    teacher_id BIGSERIAL PRIMARY KEY,
+
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    middle_name VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+);
+
+CREATE TABLE subject(
+    subject_id BIGSERIAL PRIMARY KEY,
+    name_subject VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+);
+CREATE TABLE  lesson(
+    lesson_Id BIGSERIAL PRIMARY KEY,
+    lesson_date DATE NOT NULL,
+    pair_number INT NOT NULL,
+
+    teacher_id BIGINT NOT NULL REFERENCES teacher(teacher_id),
+    group_id BIGINT NOT NULL REFERENCES student_group(group_id),
+    subject_id BIGINT NOT NULL REFERENCES subject(subject_id),
+
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+);
+CREATE TABLE lesson_attendance(
+    lesson_attendance_id BIGSERIAL PRIMARY KEY, 
+   
+    lesson_id BIGINT NOT NULL REFERENCES lesson(lesson_id) ON DELETE CASCADE,
+    student_id BIGINT NOT NULL REFERENCES student(student_id),
+
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+);
+
